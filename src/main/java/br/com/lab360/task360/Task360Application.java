83cd6d68fb8b54b507629926f24d5e73eb3dd555
Felizardodev <suelenfelizardo@icloud.com>
2023-10-26 13:21:27 -0300

    Criação do projeto inicial

---
+++ src/main/java/br/com/lab360/task360/Task360Application.java
@@ -0,0 +1,13 @@
+package br.com.lab360.task360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task360Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Task360Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lab360/task360/Task360ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lab360.task360;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Task360ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
